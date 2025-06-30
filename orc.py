66bb837b0043e5ab75bfb359588521bbfb5d459a
LucasLi <22400616@acbel.com>
2025-06-30 17:10:28 +0800

    feat: 添加新的图像处理功能
    - 添加了一个名为 `nothing` 的空函数，用于与 OpenCV 的轨迹栏回调函数兼容。
    - 新增了一个 `binary_pic` 函数，该函数读取图像并调整其大小，然后返回二值化的图像。
    - 新增了一个 `dyn_bin_pic` 函数，该函数允许用户通过动态调整阈值来二值化图像。
    - 修改了 `deal_pic` 函数，使其返回二值化的图像。
    - 识别文字的代码现在使用 `binary_pic` 函数返回的图像，并添加了 `--psm 1` 参数以优化文字识别。
    - 添加了一个包含多种语言训练数据信息的 CSV 文件，其中详细列出了每种语言的代码、名称及其对应的训练数据文件。

--- orc.py
+++ orc.py
@@ -6,6 +6,10 @@ import cv2
 from PIL import Image, ImageDraw, ImageFont
 
 
+def nothing(x):
+    pass
+
+
 def deal_pic(png):
     # 读取图像文件
     img_gray = cv2.imread(png, cv2.IMREAD_GRAYSCALE)
@@ -17,14 +21,46 @@ def deal_pic(png):
     )
     cv2.imshow(f"{png}-imgGray", img_gray)
     cv2.imshow(f"{png}-binary_adaptive", binary_adaptive)
+    # img_resize = cv2.resize(img_gray, None, fx=0.15, fy=0.15)
+    # binary_resize = cv2.adaptiveThreshold(
+    #     img_resize, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 45, 11
+    # )
+    # cv2.imshow(f"{png}-imgGray", img_resize)
+    # cv2.imshow(f"{png}-binary_resize", binary_resize)
     return binary_adaptive
 
 
-# deal_pic("Pics/UUT.png")
-# res1 = deal_pic("Pics/standard.png")
+def binary_pic(img):
+    img_gray = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+    img_gray = cv2.resize(img_gray, None, fx=1.5, fy=1.5)
+    # _, img_gray = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    cv2.imshow(f"binary_img", img_gray)
+    return img_gray
+
+
+def dyn_bin_pic(img):
+    # 创建一个黑色图像，一个窗口
+    cv2.namedWindow("settings")
+    # 创建一个改变颜色的轨迹栏
+    cv2.createTrackbar("thresh", "settings", 0, 255, nothing)
+    while True:
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+        thresh = cv2.getTrackbarPos("thresh", "settings")
+        _, binary_img = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)
+        cv2.imshow(f"binary_img", binary_img)
+    return binary_img
+
+
+# img_gray = cv2.imread("Pics/standard.png", cv2.IMREAD_GRAYSCALE)
+# res1 = dyn_bin_pic(img_gray)
+
+# deal_pic("Pics/UUT-2.jpg")
+res1 = binary_pic("Pics/standard.png")
 
 # 识别文字
-text = pytesseract.image_to_string("Pics/UUT.png", lang="eng")
+text = pytesseract.image_to_string(res1, lang="eng", config="--psm 1")
 print(text)
 
 cv2.waitKey(0)
